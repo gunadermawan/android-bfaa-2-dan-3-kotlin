eb18c2b243ff5b61694b1606d15b8407198dcea3
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-13 10:06:01 +0700

    membuat activity DetailUserActivity.kt

--- app/src/main/java/com/gunder/github/api/Api.kt
+++ app/src/main/java/com/gunder/github/api/Api.kt
@@ -1,9 +1,12 @@
 package com.gunder.github.api
 
+import com.gunder.github.data.model.DetailUserResponse
+import com.gunder.github.data.model.User
 import com.gunder.github.data.model.UserResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Api {
@@ -12,4 +15,22 @@ interface Api {
     fun getSearchUsers(
         @Query("q") query: String
     ): Call<UserResponse>
+
+    @GET("user/{username}")
+    @Headers("Authorization: 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")
+    fun getDetailUser(
+        @Path("username") username: String
+    ): Call<DetailUserResponse>
+
+    @GET("user/{username}/followers")
+    @Headers("Authorization: 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")
+    fun getFollowersUser(
+        @Path("username") username: String
+    ):Call<ArrayList<User>>
+
+    @GET("user/{username}/following")
+    @Headers("Authorization: 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")
+    fun getFollowingUser(
+        @Path("username") username: String
+    ):Call<ArrayList<User>>
 }
