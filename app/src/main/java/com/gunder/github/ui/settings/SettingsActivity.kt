0241c0bfedbc35e7255ccef96ccc96b627ac843c
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-24 20:49:33 +0700

    menambah fitur share

--- app/src/main/java/com/gunder/github/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/gunder/github/ui/settings/SettingsActivity.kt
@@ -1,11 +1,13 @@
 package com.gunder.github.ui.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gunder.github.data.model.Reminder
 import com.gunder.github.data.preferences.ReminderPreferences
 import com.gunder.github.databinding.ActivitySettingsBinding
 import com.gunder.github.receiver.AlarmReminder
+
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
@@ -17,17 +19,18 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.apply {
-            title = "Settings"
+            title = "Pengaturan"
             setDisplayHomeAsUpEnabled(true)
         }
 
+//        reminder
         val reminderPreferences = ReminderPreferences(this)
         binding.switchReminder.isChecked = reminderPreferences.getReminder().isReminder
         alarmReminder = AlarmReminder()
         binding.switchReminder.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 saveReminder(true)
-                alarmReminder.setRepeatAlarm(this, "RepeatingAlarm ", "04:53", "Github Reminder")
+                alarmReminder.setRepeatAlarm(this, "RepeatingAlarm ", "07:45", "Github Reminder")
             } else {
                 saveReminder(false)
                 alarmReminder.alarmCancel(this)
@@ -45,5 +48,13 @@ class SettingsActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    fun shareIntent(view: android.view.View) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_SEND
+        intent.putExtra(Intent.EXTRA_INTENT, "Hai, ayo cari user favorit kamu di github!")
+        intent.type = "text/plain"
+        startActivity(Intent.createChooser(intent, "Bagikan menggunakan:"))
     }
 }
